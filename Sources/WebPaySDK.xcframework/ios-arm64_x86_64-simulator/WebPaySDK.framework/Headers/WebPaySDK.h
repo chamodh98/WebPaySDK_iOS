9b5ec3ff45d7f33b681a3d2d7260beb8afdba600
Chamod Hettiarachchi <chamodbhanuka98@gmail.com>
2025-03-18 18:40:24 +0530

    Initial Commit

---
+++ Sources/WebPaySDK.xcframework/ios-arm64_x86_64-simulator/WebPaySDK.framework/Headers/WebPaySDK.h
@@ -0,0 +1,18 @@
+//
+//  WebPaySDK.h
+//  WebPaySDK
+//
+//  Created by Chamod Hettiarachchi on 2025-03-17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebPaySDK.
+FOUNDATION_EXPORT double WebPaySDKVersionNumber;
+
+//! Project version string for WebPaySDK.
+FOUNDATION_EXPORT const unsigned char WebPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebPaySDK/PublicHeader.h>
+
+
